bc599eb6f505024c5e00cfd224add684804d8763
camii_160999@hotmail.com <camii_160999@hotmail.com>
2019-10-12 11:06:33 -0400

    mvvm completo

---
+++ mvcAddress/appAddress/appAddress/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using appAddress.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appAddress.ViewModels
+{
+    public class MainViewModel
+    {
+        #region Properties
+        public List<Book> ListBook { get; set; }
+        #endregion
+
+        #region ViewModel
+        public BookViewModel bookViewModel { get; set; }
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+            instance = this;
+            this.bookViewModel = new BookViewModel();
+        } 
+        #endregion
+
+        #region singleton
+        private static MainViewModel instance;
+        public static MainViewModel GetInstance()
+        {
+            if (instance==null)
+            {
+                instance = new MainViewModel();
+            }
+            return (instance);
+        }
+        #endregion
+    }
+}

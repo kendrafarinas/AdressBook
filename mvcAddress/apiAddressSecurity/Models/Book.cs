8598504a9448c327ef20c5d4aa75ef80c857dcfa
kendrafarinas <kendraviera@hotmail.com>
2019-10-19 10:24:32 -0400

    Security

---
+++ mvcAddress/apiAddressSecurity/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿namespace apiAddressSecurity.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public enum TypeContact
+    {
+        telephone,
+        email,
+        facebook,
+        twitter,
+        instagram,
+        dblink
+    }
+    public class Book
+    {
+        [Key]
+        public int BookID { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public TypeContact type { get; set; }
+        [Required]
+        public string contact { get; set; }
+    }
+}
